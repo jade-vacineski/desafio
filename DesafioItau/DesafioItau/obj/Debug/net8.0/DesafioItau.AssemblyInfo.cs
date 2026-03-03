9c35ca05a91732a9e4952ae4526ef228df73706d
jade <jadenogueira8881@gmail.com>
2026-03-03 18:56:45 -0300

    feat: instala o entity framework -> dotnet add package Microsoft.EntityFrameworkCore --version 8.0.11 dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.11 dotnet add package Microsoft.EntityFrameworkCore.Tools --version 8.0.11 dotnet restore

--- DesafioItau/DesafioItau/obj/Debug/net8.0/DesafioItau.AssemblyInfo.cs
+++ DesafioItau/DesafioItau/obj/Debug/net8.0/DesafioItau.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioItau")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c328cc5b512082335add317b1ad89bc55980d52")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioItau")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioItau")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
